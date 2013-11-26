95991ce19ae301f30dea294701c1000302925fee
sk-5 <sikofitt@gmail.com>
2013-11-26 13:50:50 -0800

    Removed unnecessary code.  Drupal best practices ...

--- telnet-clients.tpl.php
+++ telnet-clients.tpl.php
@@ -12,61 +12,66 @@
 
 ?>
 <table class="telnet-table-link-block">
-        <tbody>
-               <?php if( $telnet_links['SyncTerm'] != '0' ) { ?>
-                <tr>
-                        <td class="img">
-                                <a href="http://syncterm.bbsdev.net/" target="_blank">
-                                        <img alt="SyncTerm" src="/<?php print $path ?>/images/syncterm.png" />
-                                </a>
-                        </td>
-                        <td>
-                                <a href="http://syncterm.bbsdev.net/" target="_blank">SyncTERM</a>
-                                is available for *nix/osx/win32 and a bunch of other oses.
-                        </td>
-
-                </tr>
-                <?php } ?>
-                <?php if( $telnet_links['Termix'] != '0' ) { ?>
-                <tr>
-                        <td class="img">
-                                <a href="http://sourceforge.net/projects/termix/" target="_blank">
-                                        <img class="termix" alt="mTelnet (OS/2, Win32)" src="/<?php print $path ?>/images/termix.png" />
-                                </a>
-                        </td>
-                        <td><a href="http://sourceforge.net/projects/termix/" target="_blank">Termix</a> is available for *nix</td>
-                </tr>
-                <?php } ?>
-                 <?php if( $telnet_links['mTelnet'] != '0' ) { ?>
-                <tr>
-                        <td class="img">
-                                <a href="http://mt32.bbses.info/" target="_blank">
-                                        <img alt="mTelnet (OS/2, Win32)" src="/<?php print $path ?>/images/mtel.png" />
-                                </a>
-                        </td>
-                        <td><a href="http://mt32.bbses.info/" target="_blank">mTelnet</a> is available for os2/win32.</td>
-                </tr>
-                <?php } ?>
-                <?php if( $telnet_links['Qodem'] != '0' ) { ?>
-                <tr>
-                        <td class="img">
-                                <a href="http://sourceforge.net/projects/qodem/" target="_blank">
-                                        <img alt="Qodem (*nix, OS/2, Win32)" src="/<?php print $path ?>/images/qodem.png" />
-                                </a>
-                        </td>
-                        <td><a href="http://sourceforge.net/projects/qodem/" target="_blank">Qodem</a> is available for *nix/osx/win32.</td>
-                </tr>
-                <?php } ?>
-                 <?php if( $telnet_links['QModem'] != '0' ) { ?>
-                <tr>
-                        <td class="img">
-                                <a href="http://qodem.sourceforge.net/about.html#qmodem" target="_blank">
-                                        <img alt="QModem (DOS, Windows)" src="/<?php print $path ?>/images/qmodem.png" />
-                                </a>
-                        </td>
-                        <td><a href="http://qodem.sourceforge.net/about.html#qmodem" target="_blank">QModem</a> is available for DOS/Win.</td>
-                </tr>
-                <?php } ?>
-
-        </tbody>
+  <tbody>
+  <?php if( $telnet_links['SyncTerm'] != '0' ):  ?>
+    <tr>
+      <td class="img">
+        <a href="http://syncterm.bbsdev.net/" target="_blank">
+          <img alt="SyncTerm" src="/<?php print $path ?>/images/syncterm.png" />
+        </a>
+      </td>
+      <td>
+        <a href="http://syncterm.bbsdev.net/" target="_blank">SyncTERM</a> is available for *nix/osx/win32 and a bunch of other oses.
+      </td>
+    </tr>
+  <?php endif; ?>
+  <?php if( $telnet_links['Termix'] != '0' ): ?>
+    <tr>
+      <td class="img">
+        <a href="http://sourceforge.net/projects/termix/" target="_blank">
+          <img class="termix" alt="mTelnet (OS/2, Win32)" src="/<?php print $path ?>/images/termix.png" />
+        </a>
+      </td>
+      <td>
+        <a href="http://sourceforge.net/projects/termix/" target="_blank">Termix</a> is available for *nix
+      </td>
+    </tr>
+  <?php endif; ?>
+  <?php if( $telnet_links['mTelnet'] != '0' ): ?>
+    <tr>
+      <td class="img">
+        <a href="http://mt32.bbses.info/" target="_blank">
+          <img alt="mTelnet (OS/2, Win32)" src="/<?php print $path ?>/images/mtel.png" />
+        </a>
+      </td>
+      <td>
+        <a href="http://mt32.bbses.info/" target="_blank">mTelnet</a> is available for os2/win32.
+      </td>
+    </tr>
+  <?php endif; ?>
+  <?php if( $telnet_links['Qodem'] != '0' ): ?>
+    <tr>
+      <td class="img">
+        <a href="http://sourceforge.net/projects/qodem/" target="_blank">
+          <img alt="Qodem (*nix, OS/2, Win32)" src="/<?php print $path ?>/images/qodem.png" />
+        </a>
+      </td>
+      <td>
+        <a href="http://sourceforge.net/projects/qodem/" target="_blank">Qodem</a> is available for *nix/osx/win32.
+      </td>
+    </tr>
+  <?php endif; ?>
+  <?php if( $telnet_links['QModem'] != '0' ): ?>
+    <tr>
+      <td lass="img">
+        <a href="http://qodem.sourceforge.net/about.html#qmodem" target="_blank">
+          <img alt="QModem (DOS, Windows)" src="/<?php print $path ?>/images/qmodem.png" />
+        </a>
+      </td>
+      <td>
+        <a href="http://qodem.sourceforge.net/about.html#qmodem" target="_blank">QModem</a> is available for DOS/Win.
+      </td>
+    </tr>
+    <?php endif; ?>
+  </tbody>
 </table>
